6abfa805a3bba6ee847f557fe2f50b54f7107948
Administrator <admin@local.host>
2014-07-24 14:18:08 -0300

    solving test problems

--- src/test/java/com/zupcat/AllTests.java
+++ src/test/java/com/zupcat/AllTests.java
@@ -1,12 +1,13 @@
 package com.zupcat;
 
+import com.zupcat.dao.DAOTests;
 import com.zupcat.dao.ObjectHolderTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-//        DAOTests.class
+        DAOTests.class,
         ObjectHolderTests.class
 })
 public class AllTests {
