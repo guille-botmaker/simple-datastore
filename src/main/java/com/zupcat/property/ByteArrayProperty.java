306d66f10c938277429255b4bd6c9dafade4b0e3
Administrator <admin@local.host>
2014-09-20 02:09:25 -0300

    better toString

--- src/main/java/com/zupcat/property/ByteArrayProperty.java
+++ src/main/java/com/zupcat/property/ByteArrayProperty.java
@@ -3,26 +3,41 @@ package com.zupcat.property;
 import com.zupcat.model.DatastoreEntity;
 import com.zupcat.model.ObjectVar;
 import com.zupcat.model.config.PropertyMeta;
+import org.apache.commons.codec.binary.Base64;
 
 import java.io.Serializable;
-import java.nio.charset.Charset;
 
 public class ByteArrayProperty extends PropertyMeta<byte[]> implements Serializable {
 
     private static final long serialVersionUID = 6181606486836703354L;
 
+    private transient byte[] cache;
 
     public ByteArrayProperty(final DatastoreEntity owner) {
         super(owner);
     }
 
     protected byte[] getValueImpl(final ObjectVar objectVar) {
-        final String result = objectVar.getString(name);
+        if (cache == null) {
+            final String result = objectVar.getString(name);
 
-        return result == null ? null : result.getBytes(Charset.forName("UTF-8"));
+            cache = result == null ? null : Base64.decodeBase64(result);
+        }
+        return cache;
     }
 
     protected void setValueImpl(final byte[] value, final ObjectVar objectVar) {
-        objectVar.set(name, value == null ? null : new String(value, Charset.forName("UTF-8")));
+        cache = value;
+    }
+
+    @Override
+    public void commit() {
+        super.commit();
+
+        final byte[] value = get();
+
+        final ObjectVar objectVar = getOwner().getInternalObjectHolder().getObjectVar();
+
+        objectVar.set(name, value == null ? null : Base64.encodeBase64String(value));
     }
 }
