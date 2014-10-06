ef9a41090adbc1e1419e5f12ede7f466ff37e0e5
Administrator <admin@local.host>
2014-10-06 19:18:01 -0300

    jsonifying

--- src/main/java/com/zupcat/model/WithIdDataObject.java
+++ src/main/java/com/zupcat/model/WithIdDataObject.java
@@ -21,6 +21,10 @@ public class WithIdDataObject extends DataObject {
         super(map);
     }
 
+    public WithIdDataObject(final WithIdDataObject another) {
+        super(another);
+    }
+
     public WithIdDataObject(final String source) throws JSONException {
         super(source);
     }
