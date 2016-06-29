3a5562d9a142b2ddfc5ca716f13016344ae96a7c
hernanliendo <hernan.liendo@gmail.com>
2016-06-29 20:17:40 -0300

    little json support to avoid dataobjects

--- src/main/java/io/botmaker/simpleredis/property/StringProperty.java
+++ src/main/java/io/botmaker/simpleredis/property/StringProperty.java
@@ -1,14 +1,21 @@
 package io.botmaker.simpleredis.property;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.botmaker.simpleredis.model.DataObject;
 import io.botmaker.simpleredis.model.RedisEntity;
 import io.botmaker.simpleredis.model.config.PropertyMeta;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringReader;
+import java.io.StringWriter;
 
 public class StringProperty extends PropertyMeta<String> implements Serializable {
 
     private static final long serialVersionUID = 6181606486836703354L;
+    private static final JsonFactory jsonFactory = new JsonFactory();
+    private static final ObjectMapper mapper = new ObjectMapper(jsonFactory);
 
     public StringProperty(final RedisEntity owner) {
         super(owner);
@@ -31,5 +38,33 @@ public class StringProperty extends PropertyMeta<String> implements Serializable
         final String s = this.get();
 
         return s != null && s.trim().length() > 0;
+    }
+
+    public <T> T load(final Class<T> type) {
+        final String s = this.get();
+
+        if (s != null) {
+            try {
+                return mapper.readValue(jsonFactory.createParser(new StringReader(s)), mapper.getTypeFactory().constructType(type));
+            } catch (final IOException e) {
+                // nothing to do (exception could not happen)
+            }
+        }
+        return null;
+    }
+
+    public void save(final Object o) {
+        String s = null;
+
+        if (o != null) {
+            final StringWriter stringWriter = new StringWriter(500);
+            try {
+                mapper.writeValue(stringWriter, o);
+            } catch (final IOException e) {
+                // nothing to do (exception could not happen)
+            }
+            s = stringWriter.toString();
+        }
+        this.set(s);
     }
 }
