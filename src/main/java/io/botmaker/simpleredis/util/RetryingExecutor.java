585380746c3a685560cc9ba7af5174a0035cedb1
hernanliendo <hernan.liendo@gmail.com>
2016-12-04 09:12:54 -0300

    menos log en retrying handler

--- src/main/java/io/botmaker/simpleredis/util/RetryingExecutor.java
+++ src/main/java/io/botmaker/simpleredis/util/RetryingExecutor.java
@@ -4,15 +4,11 @@ import io.botmaker.simpleredis.dao.RetryingHandler;
 import io.botmaker.simpleredis.exception.NoMoreRetriesException;
 import org.apache.commons.collections4.Closure;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  * Helper class that encapsulates a retrying algorithm. Usefull for AppEngine API calls
  */
 public final class RetryingExecutor<TPARAM> {
 
-    private static final Logger log = Logger.getLogger(RetryingExecutor.class.getName());
     private final int timeBetweenRetries;
     private final Closure<TPARAM> closure;
     private final TPARAM params;
@@ -43,12 +39,8 @@ public final class RetryingExecutor<TPARAM> {
     private void handleError(final Exception exception) {
         maxRetries = maxRetries - 1;
 
-        if (maxRetries == 0) {
-            log.log(Level.SEVERE, "No more tries for closure [" + closure.getClass().getName() + "]: " + exception.getMessage(), exception);
+        if (maxRetries == 0)
             throw new NoMoreRetriesException(exception);
-        }
-
-        log.log(Level.INFO, "Retrying closure [" + closure.getClass().getName() + "]: " + exception.getMessage(), exception);
 
         RetryingHandler.sleep(timeBetweenRetries);
     }
